5b911011556faeae863993d6fc8cfc149f7786ff
Botabay <tohgl49@gmail.com>
2023-02-27 11:36:06 +0600

    added a task "Localize The Barycenter of a Triangle"

--- 01.js
+++ 01.js
@@ -10,3 +10,7 @@ function chromosomeCheck(sperm) {
     "Congratulations! You're going to have a son."
 }
 
+//Localize The Barycenter of a Triangle
+function barTriang(p1, p2, p3){
+  return [+((p1[0]+p2[0]+p3[0])/3).toFixed(4), +((p1[1]+p2[1]+p3[1])/3).toFixed(4)]
+}
