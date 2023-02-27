dafeea1cabec26631491f4c7a84d38f2280cbd85
Botabay <tohgl49@gmail.com>
2023-02-27 11:35:11 +0600

    added a task "Multiply"

--- 01.js
+++ 01.js
@@ -1,5 +1,12 @@
+//Multiply
+function multiply(a, b){
+  return a * b
+}
+
+//Determine offspring sex based on genes XX and XY chromosomes
 function chromosomeCheck(sperm) {
   if (sperm!='XX' & sperm!='XY') return 'wrong data'
   return (sperm=='XX') ? "Congratulations! You're going to have a daughter." :
     "Congratulations! You're going to have a son."
 }
+
