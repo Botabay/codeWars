77013dfb46460bd231746e64f64253b1cc3c7472
Botabay <tohgl49@gmail.com>
2023-02-28 22:06:06 +0600

    task "Function 1 - hello world"

--- 01.js
+++ 01.js
@@ -63,3 +63,9 @@ function xor(a, b) {
   // TODO: Program Me
   return a === b ? false : true;
 }
+
+//Function 1 - hello world
+// Write a function "greet" that returns "hello world!"
+function greet(){
+  return "hello world!"
+}
