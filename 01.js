025ac677abf8adff726eedff3d03ef7bc944ec95
Botabay <tohgl49@gmail.com>
2023-03-01 22:42:15 +0600

    task "return to sanity"

--- 01.js
+++ 01.js
@@ -102,3 +102,10 @@ function distanceBetweenPoints(a, b) {
 function evenOrOdd(number) {
   return number % 2==0 ? 'Even' : 'Odd';
 }
+
+//Return to Sanity
+function mystery() {
+  var results =
+    {sanity: 'Hello'};
+  return    results;
+}
