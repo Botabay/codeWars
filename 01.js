a2f1ec687e1ff8f7e6ff1a048bb217fba7c4737e
Botabay <tohgl49@gmail.com>
2023-03-02 10:13:02 +0600

    task "Fun with ES6 Classes #1 - People, people, people"

--- 01.js
+++ 01.js
@@ -129,3 +129,22 @@ function Hero (name='Hero') {
     experience:0
   }
 }
+
+//Fun with ES6 Classes #1 - People, people, people
+class Person {
+  // Get coding in ES6 :D
+  constructor(firstName='John',lastName='Doe',age=0,gender='Male'){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.age=age;
+    this.gender=gender;
+  }
+  
+  sayFullName () {
+    return this.firstName+' '+this.lastName;
+  }
+  
+  static greetExtraTerrestrials (raceName) {
+    return "Welcome to Planet Earth"+' '+raceName;
+  }
+}
