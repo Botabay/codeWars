06a3ded26ac3f0e5aa4c1d9951999c67d55c8668
Botabay <tohgl49@gmail.com>
2023-03-07 11:35:58 +0600

    task "Collatz Conjecture"

--- 01.js
+++ 01.js
@@ -265,3 +265,14 @@ function shortcut (string) {
   })
   return result.join('');
 }
+
+//Collatz Conjecture (3n+1)
+var hotpo = function(n){
+    if(n == 0 || n == 1) return 0; //Optional Handler to n = 0
+    let q = 0;
+    while(n > 1){
+      q++;
+      if(n % 2 == 0) n = n / 2; else n = 3 * n + 1;
+    }
+    return q;
+}
