fe9ef356a0f840df8eb0971e37e87fb86bc6cf36
Botabay <tohgl49@gmail.com>
2023-03-09 12:23:29 +0600

    task "Training JS #10: loop statement --for"

--- 01.js
+++ 01.js
@@ -326,3 +326,13 @@ function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin){
   let result=(dolphin) ? 2:1;
   return pontoonDistance/youSpeed> sharkDistance/sharkSpeed*result ?  "Shark Bait!" : "Alive!";
 }
+
+//Training JS #10: loop statement --for
+function pickIt(arr){
+  let odd=[],even=[];
+  for(let i=0; i<arr.length;i++){
+    if(arr[i]%2==0) even.push(arr[i]);
+    else odd.push(arr[i])
+  }
+  return [odd,even];
+}
