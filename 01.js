649aa363e988d25485dc55fdd331df5bc39b5133
Botabay <tohgl49@gmail.com>
2023-03-10 14:48:46 +0600

    task "Training JS #12: loop statement --for..in and for..of"

--- 01.js
+++ 01.js
@@ -336,3 +336,13 @@ function pickIt(arr){
   }
   return [odd,even];
 }
+
+//Training JS #12: loop statement --for..in and for..of
+function giveMeFive(obj){
+  let arr=[];
+  for(let key in obj){
+    if(key.length==5) arr.push(key);
+    if(obj[key].length==5) arr.push(obj[key]);
+  }
+  return arr;
+}
