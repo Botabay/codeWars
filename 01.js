298f8d347474a9820f4ec571169c6e51d9dfc6b9
Botabay <tohgl49@gmail.com>
2023-03-16 13:30:31 +0600

    task "Find Maximum and Minimum Values of a List"

--- 01.js
+++ 01.js
@@ -791,3 +791,12 @@ function findMultiples(integer, limit) {
   }
   return arr;
 }
+
+//Find Maximum and Minimum Values of a List
+var min = function(list){    
+    return Math.min(...list);
+}
+
+var max = function(list){    
+    return Math.max(...list);
+}
