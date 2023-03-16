6df60efc0f8dfaf132ed832255648945862abcae
Botabay <tohgl49@gmail.com>
2023-03-16 13:37:07 +0600

    task "Training JS #7: if..else and ternary operator"

--- 01.js
+++ 01.js
@@ -828,3 +828,8 @@ String.prototype.camelCase=function(){
 function removeExclamationMarks(s) {
   return s.split('!').join('');
 }
+
+//Training JS #7: if..else and ternary operator
+function saleHotdogs(n){
+  return n<5? n*100: (n>=5&&n<10) ? n*95:n*90
+}
