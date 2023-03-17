4c9bdb6f1d0f27fe4f9a130a7b6515b836c5f920
Botabay <tohgl49@gmail.com>
2023-03-17 11:12:10 +0600

    task "Total pressure calculation"

--- 01.js
+++ 01.js
@@ -859,3 +859,8 @@ function uefaEuro2016(teams, scores){
   if (scores[0]===scores[1]) return `At match ${a} - ${b}, teams played draw.`
   return `At match ${a} - ${b}, ${s} won!`
 }
+
+//Total pressure calculation
+const solution= (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
+  return (givenMass1/molarMass1+givenMass2/molarMass2)*0.082*(temp+273.15)/volume;
+}
