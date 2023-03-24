afdc784647d5955a2a03f9617dd2c7c00e40d290
Botabay <tohgl49@gmail.com>
2023-03-24 23:45:19 +0600

    task "Drink about"

--- 01.js
+++ 01.js
@@ -967,3 +967,8 @@ function array(arr){
 var a = "dev"
 var b = "Lab"
 var name = a + b
+
+//Drink about
+function peopleWithAgeDrink(old) {
+  return old>=21?"drink whisky":old>=18?"drink beer":old>=14?"drink coke":"drink toddy";
+};
