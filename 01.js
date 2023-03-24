d02024d7784597c504a5447e9a9883c52584bf0e
Botabay <tohgl49@gmail.com>
2023-03-24 23:48:54 +0600

    task "Students Final Grade"

--- 01.js
+++ 01.js
@@ -977,3 +977,8 @@ function peopleWithAgeDrink(old) {
 function sixToast(num) {
   return Math.abs(6-num);
 }
+
+//Student's Final Grade
+function finalGrade (exam, projects) {
+  return (exam>90 || projects>10)? 100:(exam>75 && projects>=5)? 90:(exam>50 && projects>=2)?75:0
+}
